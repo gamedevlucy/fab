d4b5e8e8e5c52f7ca4c038f0c7810e6d235ba47f
Jason Eala <11766330+jreala@users.noreply.github.com>
2018-10-27 17:15:51 -0700

    Create transform and vector class

---
+++ Fab/src/Vector3.cpp
@@ -0,0 +1,84 @@
+#include "pch.h"
+#include "Vector3.h"
+
+Vector3::Vector3(float xx, float yy, float zz) : x(xx), y(yy), z(zz)
+{
+}
+
+Vector3::Vector3(float init[3]) 
+	: x(init[0])
+	, y(init[1])
+	, z(init[2])
+{
+}
+
+Vector3::~Vector3() = default;
+
+Vector3::Vector3(const Vector3& copy) : x(copy.x), y(copy.y), z(copy.z)
+{
+}
+
+Vector3 Vector3::operator+(const Vector3& vector)
+{
+	return Vector3(x + vector.x, y + vector.y, z + vector.z);
+}
+
+Vector3 Vector3::operator-(const Vector3& vector)
+{
+	return Vector3(x - vector.x, y - vector.y, z - vector.z);
+}
+
+Vector3 Vector3::operator*(const float scalar)
+{
+	return Vector3(x * scalar, y * scalar, z * scalar);
+}
+
+Vector3 Vector3::operator/(const float scalar)
+{
+	return Vector3(x / scalar, y / scalar, z / scalar);
+}
+
+Vector3& Vector3::operator=(const Vector3& vector)
+{
+	x = vector.x;
+	y = vector.y;
+	z = vector.z;
+	return *this;
+}
+
+Vector3& Vector3::operator+=(const Vector3& vector)
+{
+	x = x + vector.x;
+	y = y + vector.y;
+	z = z + vector.z;
+	return *this;
+}
+
+Vector3& Vector3::operator-=(const Vector3& vector)
+{
+	x = x - vector.x;
+	y = y - vector.y;
+	z = z - vector.z;
+	return *this;
+}
+
+Vector3& Vector3::operator*=(const float scalar)
+{
+	x = x * scalar;
+	y = y * scalar;
+	z = z * scalar;
+	return *this;
+}
+
+Vector3& Vector3::operator/=(const float scalar)
+{
+	x = x / scalar;
+	y = y / scalar;
+	z = z / scalar;
+	return *this;
+}
+
+bool Vector3::operator==(const Vector3& vector)
+{
+	return x == vector.x && y == vector.y && z == vector.z;
+}
---
+++ Fab/src/Vector3.h
@@ -0,0 +1,22 @@
+#pragma once
+class Vector3
+{
+public:
+	Vector3(float x, float y, float z);
+	Vector3(float init[3]);
+	Vector3(const Vector3& copy);
+	~Vector3();
+	
+	float x, y, z;
+
+	Vector3 operator + (const Vector3& v2);
+	Vector3 operator - (const Vector3& v2);
+	Vector3 operator * (const float scalar);
+	Vector3 operator / (const float scalar);
+	Vector3& operator = (const Vector3& v2);
+	Vector3& operator += (const Vector3& v2);
+	Vector3& operator -= (const Vector3& v2);
+	Vector3& operator *= (const float scalar);
+	Vector3& operator /= (const float scalar);
+	bool operator== (const Vector3& v2);
+};
